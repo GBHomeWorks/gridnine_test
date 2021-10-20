f1a86dd4ba90c6ee8301803213b0b44381961c95
Alexander <a.a.mikhaylov2015@gmail.com>
2021-10-20 15:16:51 +0300

    add classes

--- src/com/gridnine/testing/Filter.java
+++ src/com/gridnine/testing/Filter.java
@@ -1,4 +1,7 @@
 package com.gridnine.testing;
 
 public interface Filter {
+    default void baseFilter() {}
+    default void fromNowFilter() {}
+    default void timeGapFilter() {}
 }
--- src/com/gridnine/testing/Test.java
+++ src/com/gridnine/testing/Test.java
@@ -1,4 +1,11 @@
 package com.gridnine.testing;
 
+
 public class Test {
+    public static void main(String[] args) {
+
+        FlightBuilder.createFlights();
+
+    }
+
 }
