c32d320b356ad7eec4ca44c1731fba9c31f9d4a1
Alexander <a.a.mikhaylov2015@gmail.com>
2021-10-21 02:04:43 +0300

    filter wrote

--- src/com/gridnine/testing/FilterChain.java
+++ src/com/gridnine/testing/FilterChain.java
@@ -1,28 +1,31 @@
 package com.gridnine.testing;
 
+//import java.time.LocalDateTime;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 
 public class FilterChain implements Filter {
     static boolean fromNow;
     static long gapTime;
-    int segmentsArrCapacity;
-    int flightsArrCapacity;
+    static int segmentsArrCapacity;
+    static int flightsArrCapacity;
 
-    FilterChain(boolean fromNow, long gapTime, int segmentsArrCapacity, int flightsArrCapacity){
+    FilterChain(boolean fromNow, long gapTime, int segmentsArrCapacity, int flightsArrCapacity) {
         this.fromNow = fromNow;
         this.gapTime = gapTime;
         this.segmentsArrCapacity = segmentsArrCapacity;
         this.flightsArrCapacity = flightsArrCapacity;
     }
+
     public static void baseFilter(List<Flight> fls) {
-        List<Flight> flights = new ArrayList<>(500);
+        List<Flight> flights = new ArrayList<>(flightsArrCapacity);
 
         for (int i = 0; i < fls.size(); i++) {
-            List<Segment> segments = new ArrayList<>(250);
+            List<Segment> segments = new ArrayList<>(segmentsArrCapacity);
             fls.get(i).getSegments().parallelStream().
                     distinct().
                     filter(arr -> arr.getArrivalDate() != null).
@@ -31,13 +34,62 @@ public class FilterChain implements Filter {
                     forEach(segment -> segments.add(segment));
             flights.add(new Flight(segments));
         }
+        System.out.println("by baseFilter method:");
         System.out.println(flights);
+        System.out.println("----------------------------------------------------------");
+        fromNowFilter(flights);
     }
 
-    public void fromNowFilter() {
+    public static void fromNowFilter(List<Flight> fls) {
+        if (fromNow) {
+            List<Flight> flights = new ArrayList<>(flightsArrCapacity);
+
+            for (int i = 0; i < fls.size(); i++) {
+                List<Segment> segments = new ArrayList<>(segmentsArrCapacity);
+                fls.get(i).getSegments().parallelStream().
+                        filter(segment -> segment.getDepartureDate().isAfter(LocalDateTime.now())).
+                        forEach(segment -> segments.add(segment));
+                flights.add(new Flight(segments));
+            }
+            System.out.println("by fromNowFilter method:");
+            System.out.println(flights);
+            System.out.println("----------------------------------------------------------");
+            gapTimeFilter(flights);
+        }
     }
 
-    public void timeGapFilter() {
+    public static void gapTimeFilter(List<Flight> fls) {
+        if (gapTime >= 0) {
+            List<Flight> flights = new ArrayList<>(flightsArrCapacity);
+
+            for (int i = 0; i < fls.size(); i++) {
+                List<Segment> segments = new ArrayList<>(segmentsArrCapacity);
+//  не удалось применить reduce
+//                segs.add(fls.get(i).getSegments().parallelStream().reduce((segment, segment2) -> {
+//                    if ((segment.getArrivalDate().isAfter(segment2.getDepartureDate().minusHours(gapTime))) ||
+//                            (segment.getArrivalDate().isEqual(segment2.getDepartureDate().minusHours(gapTime))))
+//                        return segment;
+//                }));
+//  не удалось применить reduce
+//                segments.add(fls.get(i).getSegments().parallelStream().reduce(null, (segment, segment2) -> {
+//                    if ((segment.getArrivalDate().isAfter(segment2.getDepartureDate().minusHours(gapTime))) ||
+//                            (segment.getArrivalDate().isEqual(segment2.getDepartureDate().minusHours(gapTime))))
+//                        return new Segment(segment2.getDepartureDate(), segment2.getArrivalDate());
+//                }));
+
+                for (int j = 0; j < fls.get(i).getSegments().size() -1; j++){
+                    if(fls.get(i).getSegments().get(j).getArrivalDate().isAfter(fls.get(i).getSegments().get(j + 1).getDepartureDate().minusHours(gapTime)) ||
+                            fls.get(i).getSegments().get(j).getArrivalDate().isEqual(fls.get(i).getSegments().get(j + 1).getDepartureDate().minusHours(gapTime)) ){
+                        segments.add(fls.get(i).getSegments().get(j));
+                    }
+                }
+
+                flights.add(new Flight(segments));
+            }
+            System.out.println("by gapTimeFilter method:");
+            System.out.println(flights);
+            System.out.println("----------------------------------------------------------");
+        }
     }
 
 }
--- src/com/gridnine/testing/Test.java
+++ src/com/gridnine/testing/Test.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
+        boolean fromNow = true;
+        long gapTime = 2;
+        int segmentsArrCapacity = 500;
+        int flightsArrCapacity = 250;
 
         List<Flight> flight = FlightBuilder.createFlights();
-        FilterChain filterChain = new FilterChain(true, 2, 500, 250);
+        FilterChain filterChain = new FilterChain(fromNow, gapTime, segmentsArrCapacity, flightsArrCapacity);
         filterChain.baseFilter(flight);
 
     }
